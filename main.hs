3e340236f4b950ac58ba4058f8b3b19f2095d76b
Till Brügmann <till.bruegmann@icloud.com>
2025-05-02 22:39:36 +0200

    Tags

--- main.hs
+++ main.hs
@@ -1,5 +1,28 @@
+-- Modules
 module Main where
 
--- | A simple Hello World program in Haskell.
 main :: IO ()
-main = putStrLn "Hello, Wrld!"
+main = putStrLn (makeHTML "My page title" "My page content")
+
+-- Create Document
+makeHTML :: String -> String -> String
+makeHTML title body =
+  html_
+    (head_ (title_ title) <> body_ body)
+
+-- HTML Tags
+html_ :: String -> String
+html_ = el "html"
+
+head_ :: String -> String
+head_ = el "head"
+
+body_ :: String -> String
+body_ = el "body"
+
+title_ :: String -> String
+title_ = el "title"
+
+-- Tag Template
+el :: String -> String -> String
+el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
